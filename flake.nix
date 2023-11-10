265af1aa458e0b858b1850f11ec560aa17589d3f
Evan Relf <evan@evanrelf.com>
2023-11-10 00:11:00 -0800

    Replace `shell.nix` with `flake.nix`

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "hwaas";
+
+  inputs = {
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = inputs@{ flake-utils, nixpkgs, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        crane = inputs.crane.lib.${system};
+      in
+      rec {
+        packages = rec {
+          default = hwaas;
+
+          hwaas =
+            let
+              common = {
+                src = crane.cleanCargoSource (crane.path ./.);
+                strictDeps = true;
+                buildInputs = pkgs.lib.optionals pkgs.stdenv.isDarwin [
+                  pkgs.libiconv
+                ];
+              };
+              cargoArtifacts = crane.buildDepsOnly common;
+            in
+            crane.buildPackage (common // { inherit cargoArtifacts; });
+        };
+
+        devShells.default = crane.devShell {
+          inputsFrom = [ packages.hwaas ];
+        };
+      });
+}
